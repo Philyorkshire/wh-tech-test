cff99c1b2851794562b415dc743c8506864ba3ce
Phillip Marsden <philyorkshire@philyorkshire.uk>
2017-04-24 15:30:22 +0100

    Setup basic structure

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp'),
+    cucumber = require('gulp-cucumber');
+
+
+gulp.task('cucumber-mobile', function() {
+    return gulp.src('features/*').pipe(cucumber({
+        'steps': 'step_definitions',
+        'support': 'support/*.js',
+        'format': 'pretty',
+        'tags': '@mobile'
+    }));
+});
+
+gulp.task('cucumber-desktop', function() {
+    return gulp.src('features/*').pipe(cucumber({
+        'steps': 'step_definitions',
+        'support': 'support/*.js',
+        'format': 'pretty',
+        'tags': '@desktop'
+    }));
+});
